c0dfe1749769b2e1f61fc610ad886f0c5effdd9e
david-asem <ddy.asem@gmail.com>
2021-10-08 23:59:22 +0000

    added funtionality to allow adding a book and clearing the fields

---
+++ appes5.js
@@ -0,0 +1,59 @@
+//Book construtor
+function Book(title, author, isbn) {
+  this.title = title;
+this.author = author;
+this.isbn = isbn;
+}
+
+
+//UI constructor
+function UI() { }
+
+//Add book to list
+UI.prototype.addBookToList = function (book) {
+  const list = document.getElementById('book-list');
+  //create tr element
+  const row = document.createElement('tr');
+  
+  //insert cols
+  row.innerHTML = `<td>${book.title}</td>
+  <td>${book.author}</td>
+  <td>${book.isbn}</td>
+  <td><a href="#" class="delete">X</a>` ;
+  list.appendChild(row);
+}
+
+//clear fields
+UI.prototype.clearFields = function () {
+  document.getElementById('title').value = "";
+  document.getElementById('author').value = "";
+  document.getElementById('isbn ').value = "";
+}
+
+//event listeners
+document.getElementById('book-form').addEventListener('submit', submit);
+
+
+
+//submit fxn
+function submit(event) {
+  //get form values
+  const title = document.getElementById('title').value,
+    author = document.getElementById('author').value,
+    isbn = document.getElementById('isbn').value;
+  console.log(title, author, isbn);
+
+  //instantiate book
+  const book = new Book(title, author, isbn);
+
+  //instaantiate UI
+  const ui = new UI();
+
+  //add book to list
+  ui.addBookToList(book);
+
+  //clear fields
+  ui.clearFields(book);
+
+  event.preventDefault();
+}
